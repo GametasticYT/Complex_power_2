c4eee5ad73e3944cf63a248410bb41c7597a6d24
Sebastian Rüth <sebirueth@gmx.de>
2020-12-04 16:45:41 +0100

    working good rn

--- src/Complex_power_2.cpp
+++ src/Complex_power_2.cpp
@@ -2,12 +2,9 @@
 #include <cstring>
 #include <complex>
 
-#define TOTEMENSCHEN(num) std::complex<double> iternum(0, 0);
-
-
 int main() {
-    int screenWidth = 1050;
-    int screenHeight = 1050;
+    int screenWidth = 800;
+    int screenHeight = 800;
     int i;
 
 
@@ -20,9 +17,8 @@ int main() {
     double MouseX;
     double MouseY;
 
-    double modifier[2] = {0, 0};
 
-    std::complex<double> C(0, 0);
+    std::complex<double> testComplex (0, 0);
 
     std::complex<double> iter1(0, 0);
     std::complex<double> iter2(0, 0);
@@ -42,11 +38,6 @@ int main() {
     std::complex<double> iter16(0, 0);
     std::complex<double> iter17(0, 0);
     std::complex<double> iter18(0, 0);
-
-//    for (int j = 0; j < 18; j++)
-//    {
-//        TOTEMENSCHEN(j+1);
-//    }
 
 
 
@@ -82,74 +73,62 @@ int main() {
         MouseY = (GetMouseY() - (GetScreenHeight() / 2.0)) / 100.0;
 
 
-        C = {MouseX, MouseY};
 
-        // Change Iter1 Points with Buttons
-        if (IsKeyDown(KEY_LEFT))
-        {
-            modifier[0] -= 0.01;
-        } else if (IsKeyDown(KEY_RIGHT)) {
-            modifier[0] += 0.01;
-        } else if (IsKeyDown(KEY_UP)) {
-            modifier[1] -= 0.01;
-        } else if (IsKeyDown(KEY_DOWN)) {
-            modifier[1] += 0.01;
-        }
 
         // Get the Complex points
-        iter1 = {0 + modifier[0], 0 + modifier[1]};
-        iter1 = std::pow(iter1, 2) + C;
+        iter1 = {MouseX, MouseY};
+        iter1 = std::pow(iter1, 2);
 
         iter2 = iter1;
-        iter2 = std::pow(iter2, 2) + C;
+        iter2 = std::pow(iter2, 2);
 
         iter3 = iter2;
-        iter3 = std::pow(iter3, 2) + C;
+        iter3 = std::pow(iter3, 2);
 
         iter4 = iter3;
-        iter4 = std::pow(iter4, 2) + C;
+        iter4 = std::pow(iter4, 2);
 
         iter5 = iter4;
-        iter5 = std::pow(iter5, 2) + C;
+        iter5 = std::pow(iter5, 2);
 
         iter6 = iter5;
-        iter6 = std::pow(iter6, 2) + C;
+        iter6 = std::pow(iter6, 2);
 
         iter7 = iter6;
-        iter7 = std::pow(iter7, 2) + C;
+        iter7 = std::pow(iter7, 2);
 
         iter8 = iter7;
-        iter8 = std::pow(iter8, 2) + C;
+        iter8 = std::pow(iter8, 2);
 
         iter9 = iter8;
-        iter9 = std::pow(iter9, 2) + C;
+        iter9 = std::pow(iter9, 2);
 
         iter10 = iter9;
-        iter10 = std::pow(iter10, 2) + C;
+        iter10 = std::pow(iter10, 2);
 
         iter11 = iter10;
-        iter11 = std::pow(iter11, 2) + C;
+        iter11 = std::pow(iter11, 2);
 
         iter12 = iter11;
-        iter12 = std::pow(iter12, 2) + C;
+        iter12 = std::pow(iter12, 2);
 
         iter13 = iter12;
-        iter13 = std::pow(iter13, 2) + C;
+        iter13 = std::pow(iter13, 2);
 
         iter14 = iter13;
-        iter14 = std::pow(iter14, 2) + C;
+        iter14 = std::pow(iter14, 2);
 
         iter15 = iter14;
-        iter15 = std::pow(iter15, 2) + C;
+        iter15 = std::pow(iter15, 2);
 
         iter16 = iter15;
-        iter16 = std::pow(iter16, 2) + C;
+        iter16 = std::pow(iter16, 2);
 
         iter17 = iter16;
-        iter17 = std::pow(iter17, 2) + C;
+        iter17 = std::pow(iter17, 2);
 
         iter18 = iter17;
-        iter18 = std::pow(iter18, 2) + C;
+        iter18 = std::pow(iter18, 2);
 
 
         //Draw lines between all complex points
@@ -175,9 +154,6 @@ int main() {
 
         // Conversions from string to char bc DrawText() uses char :P
         strcpy(fpsTxt, std::to_string(GetFPS()).c_str());
-
-        // Draw Modifier
-        DrawCircle(modifier[0] * 100.0, modifier[1] * 100.0, 8, RED);
 
         // Draw the points
         DrawCircle((std::real(iter1)) * 100.0, (std::imag(iter1)) * 100.0, 5, BLUE);
